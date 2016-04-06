a5c9ff01b049c36f40a2b8bb55d0027451936688
LL8054 <LL8054@Hotmail.com>
2016-04-05 23:18:01 -0500

    updated NYSE list

--- NYSE.R
+++ NYSE.R
@@ -7,10 +7,13 @@ setwd("~/LL/Trading - GPC/Primus/Reference/Stock LIsts/PrimusScripts")
 data <- data[,c(1:2,7:8)]
 data <- arrange(data, Sector, Industry)
 
+data <- data[!grepl("Energy|Health", data$Sector),]
 data <- data[!grepl(".Pharmaceuticals|Gas|Coal|Health Insurance|Biotech.|Retail Stores|Real 
-        Estate Investment Trusts|Coal|Bank.|Precious Metals|Real Estate|Homebuilding|Hotel.|Stores", data$Industry),]
+                Estate Investment Trusts|Coal|Bank.|Precious Metals|Real Estate|Homebuilding|Hotel.|Stores|
+                Savings Institutions|Insurers|Marine Transportation|Apparel|Restaurants|RETAIL.|
+                    Oil", data$Industry),]
 data <- data[!grepl("PIMCO|Shipping|Gold|Restaurant|Restaurant.|S\\.A\\.|Chile|Brasil|N\\.V\\.|
-                    S\\.A\\.B\\.|PLC|MLP|S\\.P\\.A\\.", data$Name),]
+                    S\\.A\\.B\\.|PLC|MLP|S\\.P\\.A\\.|Energy", data$Name),]
 
 
 write.xlsx2(data, "NYSE.xlsx")
